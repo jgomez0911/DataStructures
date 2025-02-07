fad2fb5bed161ddf6cc9c78a583c82e965369744
jgomez0911 <jorgegomez0911@gmail.com>
2025-02-07 11:42:13 -0600

    added the commetns to the methods to start implementing

--- Program1/ChildrenDriver.java
+++ Program1/ChildrenDriver.java
@@ -1,9 +1,21 @@
 package Program1;
 
 public class ChildrenDriver {
-    
+
+/*1-	Create an input file of at least 20 lines with at least 5 lines
+for each item type (<ItemA>, <ItemB>, <Item> that is neither <ItemA> nor <ItemB>).
+Include some duplicate lines to test that the add method does not add duplicate items.
+2-	Ask the user the name of the data file, read the file name, open the file, read the
+file line by line, and upload the data to an <Item>SortedArraySet.
+3-	Print the list. Make sure the items are in sorted order and there are no duplicates.
+4-	Include at least two test cases for each one of the following methods: indexOf, remove, grab,
+and categorySet. Make sure to print appropriate messages in the output so that it is easy to interpret your test cases.
+ */
+
     public static void main(String[] args) {
         // Create a new instance of the Children class
         Children children = new Children();
     }
+
+
 }
--- Program1/ChildrenSortedArraySet.java
+++ Program1/ChildrenSortedArraySet.java
@@ -9,32 +9,72 @@ public class ChildrenSortedArraySet {
         return this.max = max;
     }
 
+    /*Inserts an object of type <Item> in the set such that: 
+    (1) the method must maintain the sorted order according to
+    the compareTo method and (2) duplicates are not allowed which 
+    means that the item is not inserted if the set already has another
+    item that is equal to the input item where equality is determined 
+    as described above. It is not the responsibility of the collection 
+    class to decide on how to sort items or how to test the equality. 
+    Sorting and equality depend merely on the semantics of your <Item> 
+    class. Moreover, according to compareTo, the items will be sorted either 
+    in ascending or descending order. 
+    You are not allowed to use the java Arrays.sort method. */
     public void insert(Children i){
         if (i == null) {
             throw new NullPointerException("Cannot insert null into the set");
         }
     }
 
+    /*Returns the number of items in the collection. */
     public int size(){
         return 0;
     }
 
+    /*Returns a string representation of the collection
+    that includes all items. The output string must be nicely
+    formatted in a tabular format. For example, a list of Circles
+    is displayed as follows. Note the second row shows an <ItemA> 
+    object while the third row shows an <ItemB> object */
     public String toString(){
         return "[]";
     }
 
+    /*Returns an int that represents the array index at 
+    which the input <Item> is found or -1 if the <Item> 
+    is not found. The equality of items is determined by 
+    the equals method. */
     public int indexOf(Children i){
         return -1;
     }
 
+    /*Search the collection for an Item that equals the 
+    input <Item> and, if found, remove this <Item> from 
+    the set. Return true if the <Item> is removed and false 
+    otherwise. This method must maintain the sorted order.  
+    Shift the array elements down to fill the gap that is 
+    created by the deleted <Item>. . Do not use the Java's 
+    Arrays.sort(…) method. */
     public Boolean remove(Children i){
         return false;
     }
 
+    /*Just replacing the deleted item with the last element 
+    in the array will not work anymore because this may break 
+    the sorted order Return the <Item> that is stored at position 
+    index in the set or null if index is beyond the size of the
+    collection. Do not delete the element from the set. */
     public Children grab(int index){
         return null;
     }
 
+    /*Return as output an <Item>SortedArraySet that contains only
+    specific subitem type. Assume 1 represents <ItemA>, 2 represents 
+    <ItemB>, and 3 represents items that are neither <ItemA> nor <ItemB>. 
+    Then categorySet(1) returns a set of items of type <ItemA>, in an array 
+    of type <Item>. The method returns null if the input is any number other
+    than 1, 2, or 3. The method returns an empty set (not nulfl) if there are
+    no items of the given type. */
     public ChildrenSortedArraySet categorySet(int type){
         return null;
     }
