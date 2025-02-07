cd3d4009040ea0a2f1d91d3613f46f589f261927
jgomez0911 <jorgegomez0911@gmail.com>
2025-02-06 20:26:07 -0600

    added all teh files and methods that need to be completed- all methods still need work

---
+++ Program1/ChildrenDriver.java
@@ -0,0 +1,9 @@
+package Program1;
+
+public class ChildrenDriver {
+    
+    public static void main(String[] args) {
+        // Create a new instance of the Children class
+        Children children = new Children();
+    }
+}
---
+++ Program1/ChildrenSortedArraySet.java
@@ -0,0 +1,41 @@
+package Program1;
+
+public class ChildrenSortedArraySet {
+
+    private Children[] ChildrenAry;
+    private Integer max;
+
+    public Integer Children(Integer max){
+        return this.max = max;
+    }
+
+    public void insert(Children i){
+        if (i == null) {
+            throw new NullPointerException("Cannot insert null into the set");
+        }
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public String toString(){
+        return "[]";
+    }
+
+    public int indexOf(Children i){
+        return -1;
+    }
+
+    public Boolean remove(Children i){
+        return false;
+    }
+
+    public Children grab(int index){
+        return null;
+    }
+
+    public ChildrenSortedArraySet categorySet(int type){
+        return null;
+    }
+}
