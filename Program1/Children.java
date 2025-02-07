ca6353b966d9b737925431f3a6c07068424d685b
jgomez0911 <jorgegomez0911@gmail.com>
2025-02-06 18:17:50 -0600

    part 1 of program 2 completed

--- Program1/Children.java
+++ Program1/Children.java
@@ -1,13 +1,12 @@
 package Program1;
 
-
 /*
  * @author Jorge Gomez
  * @date 01/26/25
- * this is the Children class with string name and integer age attributies 
+ * this is the Children class with string name and integer age attributies
  */
 
-public class Children {
+ public class Children implements Comparable<Children> {
     // private fields
     private String name;
     private int age;
@@ -44,5 +43,12 @@ public class Children {
     @Override
     public String toString(){
         return "Name: " + this.getName() + ", Age: " + this.getAge();
+    }
+    @Override
+    public int compareTo(Children o) {
+        if (this.age != o.age){
+            return Integer.compare(this.age, o.age);
+        }
+        return this.name.compareToIgnoreCase(o.name);
     }
 }
