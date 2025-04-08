ae6c95a3005567dd9bb7e20c170d4a0af07d7909
jgomez0911 <jorgegomez0911@gmail.com>
2025-04-08 18:50:07 -0500

    BST completed

--- program5/BSTNode.java
+++ program5/BSTNode.java
@@ -1,7 +1,7 @@
 package program5;
 
 public class BSTNode {
-    
+
     private MenuItem data;
     private BSTNode left;
     private BSTNode right;
@@ -12,8 +12,39 @@ public class BSTNode {
         right = r;
     }
 
-    public void getMenuItem(){
-
+    // Getter for the MenuItem data
+    public MenuItem getMenuItem() {
+        return data;
     }
 
+    // Setter for the MenuItem data
+    public void setMenuItem(MenuItem data) {
+        this.data = data;
+    }
+
+    // Getter for the left child
+    public BSTNode getLeft() {
+        return left;
+    }
+
+    // Setter for the left child
+    public void setLeft(BSTNode left) {
+        this.left = left;
+    }
+
+    // Getter for the right child
+    public BSTNode getRight() {
+        return right;
+    }
+
+    // Setter for the right child
+    public void setRight(BSTNode right) {
+        this.right = right;
+    }
+
+    // Optional: toString method for debugging
+    @Override
+    public String toString() {
+        return data.toString(); // Uses the toString method of MenuItem
+    }
 }
